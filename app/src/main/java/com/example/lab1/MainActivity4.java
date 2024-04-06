41a8bffc7f735178e72f64fe1621a58d07965907
Daniel <a20180805@pucp.edu.pe>
2024-04-05 23:59:11 -0500

    idk

--- app/src/main/java/com/example/lab1/MainActivity4.java
+++ app/src/main/java/com/example/lab1/MainActivity4.java
@@ -2,8 +2,17 @@ package com.example.lab1;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity4 extends Activity {
+import androidx.activity.EdgeToEdge;
 
+public class MainActivity4 extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.vista4);
+
+    }
 }
